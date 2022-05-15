689c2eac630edaaa05ee420279b0cecc4555e90c
Anjala AbdulRehman <anjala.ar@fullcontact.com>
2022-05-15 12:58:49 +0530

    PostPay-Assignment-Submission

---
+++ executors/src/test/test_order_functionalities.py
@@ -0,0 +1,40 @@
+import unittest
+
+import pandas as pd
+
+from executors.src.main.main import get_payment_status, get_orders_for_time_delta, get_shop_id_count_paid_orders_90d
+
+
+class MyTestCase(unittest.TestCase):
+    data = {'order_id': ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+            'customer_id': ["a", "b", "c", "a", "d", "f", "c", "b", "d"],
+            'created_date': [pd.to_datetime('2022-05-15'), pd.to_datetime('2022-05-13'),
+                             pd.to_datetime('2022-05-14'), pd.to_datetime('2022-05-11'),
+                             pd.to_datetime('2022-05-07'), pd.to_datetime('2022-05-05'),
+                             pd.to_datetime('2022-05-10'), pd.to_datetime('2022-05-09'),
+                             pd.to_datetime('2022-05-05')],
+            'order_amount': [1000, 200, 200, 4000, 1650, 600, 700, 450, 350],
+            'shop_id': ["1000", "1001", "1000", "1003", "1002", "1001", "1002", "1003", "1001"],
+            'order_status': ["paid", "due", "unpaid", "paid", "paid", "due", "unpaid", "unpaid", "unpaid"]
+
+            }
+    test_df = pd.DataFrame(data)
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+    def test_get_payment_status(self):
+        customer_data = get_payment_status(self.test_df)
+        self.assertEqual(set(customer_data[customer_data.customer_id_has_paid == True]["customer_id"]), set("a"))
+
+    def test_get_orders_for_time_delta(self):
+        customer_data = get_orders_for_time_delta(self.test_df)
+        self.assertEqual(set(customer_data[customer_data.customer_id == "a"]["order_for_7_days"]), {2.0})
+
+    def test_get_shop_id_count_paid_orders_90d(self):
+        customer_data = get_shop_id_count_paid_orders_90d(self.test_df)
+        self.assertEqual(set(customer_data[customer_data.customer_id == "a"]["shop_id_count_paid_orders_90D"]), {2.0})
+
+
+if __name__ == '__main__':
+    unittest.main()
